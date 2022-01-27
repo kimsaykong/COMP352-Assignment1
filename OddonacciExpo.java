a5aaa72d16549c0cccdc73bb278dad692bb8b7fc
Kimsay KONG <37560327+kimsaykong@users.noreply.github.com>
2022-01-27 12:53:46 -0500

    finish java code

--- OddonacciExpo.java
+++ OddonacciExpo.java
@@ -1,6 +1,40 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class OddonacciExpo {
-    public int oddonacci(int num){
-        if (num<=3) return 1;
-        return oddonacci(num-1)+oddonacci(num-2)+oddonacci(num-3);
+    static int step=0;
+    public static void main(String[] args) {
+        step=0;
+        String content = "";
+        for (int i =1;i<=35;i=i+1){
+            long startTime = System.currentTimeMillis();
+            long oddonacciNumber = oddonacciExpo(i);
+            long endTime = System.currentTimeMillis();
+            content += String.format("Oddonacci(%d) : %d \n" +
+                    "Runtime in Second : %d \n" +
+                    "Step's count : %d \n" +
+                    "-----------------------------\n",i,oddonacciNumber,((endTime-startTime)/1000),step);
+        }
+        try{
+            writeLog(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static long oddonacciExpo(int num){
+        step++;
+        //Base case
+        if (num==1 ||num==2||num==3) return 1;
+        // Binary Recursive Call
+        return oddonacciExpo(num-1)+oddonacciExpo(num-2)+oddonacciExpo(num-3);
+    }
+    public static void writeLog(String content) throws IOException {
+        File outputFile = new File("outputExpo.txt");
+        if (!outputFile.exists())outputFile.createNewFile();
+        FileWriter writer = new FileWriter(outputFile);
+        writer.write(content);
+        writer.close();
+        System.out.println("Successfully wrote to the log file.");
     }
 }
--- OddonacciLinear.java
+++ OddonacciLinear.java
@@ -1,3 +1,41 @@
+import jdk.jfr.Unsigned;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class OddonacciLinear {
-    
+    static int step=0;
+    public static void main(String[] args) {
+        String content = "";
+        for (int i =5;i<=100;i=i+5){
+            long startTime = System.currentTimeMillis();
+            Long oddonacciNumber = oddonacciLinear(i,1,1,1);
+            long endTime = System.currentTimeMillis();
+            content += String.format("Oddonacci(%d) : %d \n" +
+                                    "Runtime in Second : %d \n" +
+                                    "Step's count : %d \n" +
+                                    "-----------------------------\n",i,oddonacciNumber,((endTime-startTime)/1000),step);
+        }
+        try{
+            writeLog(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static long oddonacciLinear(int num,int first, int second, int third){
+        step++;
+        // base case
+        if (num==1 || num==2 || num==3) return third;
+        // tail recursive call
+        return oddonacciLinear(num-1,second,third,first+second+third);
+    }
+    public static void writeLog(String content) throws IOException {
+        File outputFile = new File("outputLinear.txt");
+        if (!outputFile.exists())outputFile.createNewFile();
+        FileWriter writer = new FileWriter(outputFile);
+        writer.write(content);
+        writer.close();
+        System.out.println("Successfully wrote to the log file.");
+    }
 }
